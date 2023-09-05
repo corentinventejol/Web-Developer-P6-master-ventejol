0bb28fe0f3ff7b9cbee00c1e8a3033117d40910c
corentinventejol <97588972+corentinventejol@users.noreply.github.com>
2023-09-05 14:47:01 +0200

    changement de nom stuff en sauce

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const stuffRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 app.use(express.json());
